6ee344b2a9627823ef309f9c318f92ce0c530c37
lizeng <jsnoopyone@hotmail.com>
2019-04-10 10:35:09 +0800

    Initial commit

--- src/main/java/application/rest/HealthEndpoint.java
+++ src/main/java/application/rest/HealthEndpoint.java
@@ -7,13 +7,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class HealthEndpoint implements HealthIndicator {
 
-  @Override
-  public Health health() {
-    /*
-    if (!healthy) {
-      return Health.down().withDetail("Not healthy", 500).build();
-    }
-    */
-    return Health.up().build();
-  }
+	@Override
+	public Health health() {
+		/*
+		 * if (!healthy) { return Health.down().withDetail("Not healthy", 500).build();
+		 * }
+		 */
+		return Health.up().build();
+	}
 }
